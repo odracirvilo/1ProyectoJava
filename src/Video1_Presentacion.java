70500235902e02a190c14075dcacfd3d45e8738a
odracirvilo <ricardo.vilo.cgsdaw@gmail.com>
2023-11-04 10:26:18 +0100

    Comienzo Proyecto

---
+++ src/Video1_Presentacion.java
@@ -0,0 +1,9 @@
+
+public class Video1_Presentacion {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
---
+++ src/Video2_InstalacionJREyEclipse.java
@@ -0,0 +1,9 @@
+
+public class Video2_InstalacionJREyEclipse {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
